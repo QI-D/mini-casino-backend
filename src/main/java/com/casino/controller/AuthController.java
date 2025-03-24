ee3eb129e3b0d8f7615df700de727a49330c1067
QI-D <qid14@outlook.com>
2025-03-24 09:23:50 -0700

    add auth controller
    Signed-off-by: QI-D <qid14@outlook.com>

---
+++ src/main/java/com/casino/controller/AuthController.java
@@ -0,0 +1,50 @@
+package com.casino.controller;
+
+import com.casino.dto.LoginRequest;
+import com.casino.dto.PlayerDto;
+import com.casino.dto.Response;
+import com.casino.entity.Player;
+import com.casino.mapper.PlayerMapper;
+import com.casino.security.JwtUtils;
+import com.casino.service.PlayerService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final PlayerService playerService;
+    private final PlayerMapper playerMapper;
+    private final JwtUtils jwtUtils;
+
+    public AuthController(PlayerService playerService, PlayerMapper playerMapper, JwtUtils jwtUtils) {
+        this.playerService = playerService;
+        this.playerMapper = playerMapper;
+        this.jwtUtils = jwtUtils;
+    }
+
+    @PostMapping("/register")
+    public Response registerPlayer(@RequestBody PlayerDto playerDto) {
+        PlayerDto registeredPlayer = playerMapper.toDto(playerService.registerPlayer(playerMapper.toEntity(playerDto)));
+        return Response.builder()
+                .status(200)
+                .message("Player registered successfully")
+                .player(registeredPlayer)
+                .build();
+    }
+
+    @PostMapping("/login")
+    public Response login(@RequestBody LoginRequest loginRequest) {
+        Player player = playerService.authenticate(loginRequest.getUsername(), loginRequest.getPassword());
+
+        String token = jwtUtils.generateToken(player);
+        PlayerDto playerDto = playerMapper.toDto(player);
+
+        return Response.builder()
+                .status(200)
+                .message("Login successful")
+                .token(token)
+                .expirationTime("6 month")
+                .player(playerDto)
+                .build();
+    }
+}
