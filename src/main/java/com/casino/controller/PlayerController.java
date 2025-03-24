24b58bf282f847cc801f2d6a0464433cee3d5e0a
QI-D <qid14@outlook.com>
2025-03-24 09:24:35 -0700

    add player controller
    Signed-off-by: QI-D <qid14@outlook.com>

---
+++ src/main/java/com/casino/controller/PlayerController.java
@@ -0,0 +1,45 @@
+package com.casino.controller;
+
+import com.casino.dto.Response;
+import com.casino.service.PlayerService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/player")
+public class PlayerController {
+    private final PlayerService playerService;
+
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+
+    @PostMapping("/deposit")
+    public Response deposit(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @RequestParam double amount) {
+        String username = userDetails.getUsername();
+        double newBalance = playerService.deposit(username, amount);
+
+        return Response.builder()
+                .status(200)
+                .message("Deposit successful")
+                .balance(newBalance)
+                .build();
+    }
+
+    @GetMapping("/balance")
+    public Response getBalance(@AuthenticationPrincipal UserDetails userDetails) {
+        String username = userDetails.getUsername();
+
+        double balance = playerService.getBalance(username);
+
+        return Response.builder()
+                .status(200)
+                .message("Balance retrieved successfully")
+                .balance(balance)
+                .build();
+    }
+}
